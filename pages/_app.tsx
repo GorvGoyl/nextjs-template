4d30f99bc6b33602a96b379dda1213677e3e764e
Gourav <1gouravgg@gmail.com>
2021-10-10 20:12:30 +0530

    added sass / scss support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from "next/app";
-import "../styles/globals.css";
+import "../styles/globals.scss";
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return <Component {...pageProps} />;
 }
